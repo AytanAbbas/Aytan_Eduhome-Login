58a06a7b100b9368f667791a6289d5c67cc61680
AytanAbbas <aytanab@code.edu.az>
2021-12-15 11:00:14 +0400

    EduHome Login Added

---
+++ EduHome/EduHome/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,67 @@
+﻿using EduHome.Data;
+using EduHome.Models;
+using EduHome.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduHome.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AccountController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(AppDbContext context,UserManager<IdentityUser> userManager,SignInManager<IdentityUser> signInManager)
+        {
+           _context = context;
+           _userManager = userManager;
+           _signInManager = signInManager;
+        }
+        public IActionResult Register()
+        {
+   
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(VmRegister model)
+        {
+            if (ModelState.IsValid)
+            {
+                CustomUser newUser = new CustomUser() 
+                {
+                    Name = model.Name,
+                    Surname = model.Surname,
+                    Email = model.Email, 
+                    UserName = model.Email,
+                CreatedDate=DateTime.Now};
+                var result = await _userManager.CreateAsync(newUser, model.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(newUser, false);
+                    return RedirectToAction("Index", "About");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                    return View(model);
+                }
+            }
+
+            return View(model);
+        }
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+    }
+}
